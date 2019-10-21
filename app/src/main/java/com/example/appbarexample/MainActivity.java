16364eccb1f4e34a8a179ddee43f7199da4f85c4
ahmed.zien <ahmed.zien@gmail.com>
2019-10-21 20:51:52 +0200

    Handling the Click Events of Menu items

--- app/src/main/java/com/example/appbarexample/MainActivity.java
+++ app/src/main/java/com/example/appbarexample/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.appbarexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +25,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.app_bar_menu,menu);
+        menuInflater.inflate(R.menu.app_bar_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.action_share:
+                Toast.makeText(this, "share option selected ...", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_settings:
+                Toast.makeText(this, "settings option selected ...", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
